1521af52e3b2348dc3c3d6729c7e64f21f5c3d82
LaibaSabir1 <laibasabir571@gmail.com>
2026-07-11 18:31:53 +0500

    COT updated

--- Session 2 Tasks/COT.py
+++ Session 2 Tasks/COT.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 load_dotenv()
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-flash-latest")
 
 # ---- The 3 problems given in the assignment ----
 problems = [
